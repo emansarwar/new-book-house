492b6c7ca2545398ba9d22b71a2b51607d4f0fb4
unknown <emansarwar3@gmail.com>
2023-02-01 16:49:36 +0600

    hello

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -83,6 +83,7 @@ const Login = () => {
                 Sign Up
               </Link>
             </p>
+            <hr />
             <p>
             <SocialLogin />
             </p>
--- src/pages/Shared/SocialLogin/SocialLogin.js
+++ src/pages/Shared/SocialLogin/SocialLogin.js
@@ -21,7 +21,7 @@ const SocialLogin = () => {
         <div>
             {/* <p className='text-center'><small>Social Login</small></p> */}
             
-            <button onClick={handleGoogleSIgnIn} className=' btn btn btn-wide btn-success m'>Google Sign-In</button>
+            <button onClick={handleGoogleSIgnIn} className=' btn btn btn-wide btn-success mt-2'>Google Sign-In</button>
         
             {/* <div className="form-control mt-6">
               <input className="btn btn-primary" type="submit" value="Login" />
